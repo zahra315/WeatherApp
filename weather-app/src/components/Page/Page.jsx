a8a9df1a1e0e913758d574566eab339d828e11bc
Zahra Seyedi <za.seyedi@gmail.com>
2022-03-03 00:55:43 -0800

    how data manipulation in parent component going to hapend

--- weather-app/src/components/Page/Page.jsx
+++ weather-app/src/components/Page/Page.jsx
@@ -1,17 +1,29 @@
 import React, { Fragment } from "react";
 
 import Header from "../Header";
+import Form from "../Form";
+import Error from "../Error";
+import Loader from "../Loader";
+import Forecast from "../Forecast";
+
+import styles from "./Page.module.css";
+import useForecast from "../../hooks/useForecast";
 
 const Page = () => {
+  const { isError, isLoading, isForecast } = useForecast();
   return (
     <Fragment>
       <Header />
-      <div>
+      <div className={"${styles.box} position-relative"}>
         {/*Form */}
+        {isLoading && <Form />}
         {/*Error */}
+        {isError && <Error message={isError} />}
         {/*Loader */}
-        {/*Forecast */}
+        {isLoading && <Loader />}
       </div>
+      {/*Forecast */}
+      {isForecast && <Forecast />}
     </Fragment>
   );
 };
