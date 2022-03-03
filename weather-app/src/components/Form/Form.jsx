b822ed024b89c4590f9dceefe149148955edc617
Zahra Seyedi <za.seyedi@gmail.com>
2022-03-03 00:50:32 -0800

    on submit and on change event function for form component is set

---
+++ weather-app/src/components/Form/Form.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import propTypes from "prop-types";
+
+import styles from "./Form.module.css";
+
+const Form = ({ submitSearch }) => {
+  const [location, setLocation] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!location || location === "") return;
+    submitSearch(location);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        arial-label="location"
+        type="text"
+        className={"${styles.input} form-control"}
+        placeholder="Location"
+        required
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+
+      <button type="submit" className={styles.button} onClick={onSubmit}>
+        SEARCH
+      </button>
+    </form>
+  );
+};
+
+Form.propTypes = {
+  submitSearch: propTypes.func.isRequired,
+};
+
+export default Form;
