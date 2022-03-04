ba5dc65d0aede7e07376ffec98ecd9555be049fd
Zahra Seyedi <za.seyedi@gmail.com>
2022-03-03 23:52:47 -0800

    Error prop

---
+++ weather-app/src/components/Error/Error.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./Error.module.css";
+
+const Error = ({ message }) => {
+  return (
+    <div className={`${styles.error} alart position-absolut`} role="alert">
+      {message}
+    </div>
+  );
+};
+Error.propTypes = {
+  message: PropTypes.string,
+};
+
+Error.defaultProps = {
+  message: "AN ERROR OCCURRED",
+};
+
+export default Error;
